1d83fd3a0791aa0d8c33c9408d26e7b7593e66ac
yourcoke <yourcoke@users.noreply.github.com>
2016-07-19 09:16:30 +0200

    Google Login Cleanup
    Basicly the code from 
    https://github.com/Mila432/Pokemon_Go_API/issues/64

--- main.py
+++ main.py
@@ -18,6 +18,7 @@ try:
 	import pokemon_pb2
 	import logic
 	import dirty
+	import stops
 	import api
 	config.pub=False
 except:
@@ -30,7 +31,7 @@ def get_acces_token(usr,pws,type):
 		print '[!] Using google as login..'
 		google_data= login.login_google(usr,pws)
 		if google_data is not None:
-			access_token=google_data['id_token']
+			access_token=google_data['Auth']
 			ltype='google'
 		else:
 			access_token=None
@@ -41,25 +42,24 @@ def get_acces_token(usr,pws,type):
 	return access_token,ltype
 	
 def main():
-	if platform.system() == 'Windows':
+	if 'nux' not in platform.system():
 		os.system("title Pokemon GO API Python")
 		os.system("cls")
 	else:
-		# Catches "Lunux" and "Darwin" (OSX), among others
 		os.system("clear")
 	parser = argparse.ArgumentParser()
 	parser.add_argument("-u", "--username", help="Login", default=None)
 	parser.add_argument("-p", "--password", help="Password", default=None)
 	parser.add_argument("-t", "--type", help="Google/PTC", required=True)
 	parser.add_argument("-l", "--location", help="Location", required=True)
-	#parser.add_argument("-d", "--distance", help="Distance", required=True)
+	parser.add_argument("-d", "--distance", help="Distance", required=True)
 	args = parser.parse_args()
 	if not args.username:
 		args.username = getpass("Username: ")
 	if not args.password:
 		args.password = getpass("Password: ")
 	if 'ptc' in args.type.lower() or 'goo' in args.type.lower():
-		#config.distance=args.distance
+		config.distance=args.distance
 		access_token,ltype=get_acces_token(args.username,args.password,args.type.lower())
 		if access_token is not None:
 			if config.debug:
